38a972aaa0d372f79e47de179c0818b52b126f08
Oleks <oleks@oleks.info>
2019-03-11 20:33:12 +0100

    Include the license

--- template.sh
+++ template.sh
@@ -11,3 +11,8 @@ git-identity.sh . | \
 date -u --iso-8601=minutes | \
   prefix.sh "Generation date-time (ISO 8601): " | \
   indented-comments.sh
+
+printf "\n"
+
+perl -pe "chomp if eof" LICENSE | \
+  comments.sh
